9eb2ef7f496c0272e9e8e520ff01e99a6e24dbcd
Benoît Hubert <benoit.hubert@wildcodeschool.com>
2020-10-25 18:07:12 +0100

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React starter template</h1>
+        <p>Since React 17, you don&#39;t need this anymore:</p>
+        <pre>
+          <code>import React from &#39;react&#39;;</code>
+        </pre>
+      </header>
+    </div>
+  );
+}
+
+export default App;
