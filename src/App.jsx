173832c38c8922a4cec7dca59c5c81ef2fa86739
CASTAN <christophecastan4@gmail.com>
2020-10-27 18:21:46 +0100

    create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 import './normalize.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
