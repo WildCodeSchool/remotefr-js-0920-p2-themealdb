1a40a32afb06e3bb2675c611135a3fc0d02f8cdf
Matisse <matissefamularo@gmail.com>
2020-10-27 10:35:55 +0100

    Functionnal recipepage no css, no props.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
+import RecipePage from './components/RecipePage';
 import './normalize.css';
 
 function App() {
   return (
     <div className="App">
-
+      <RecipePage />
     </div>
   );
 }
