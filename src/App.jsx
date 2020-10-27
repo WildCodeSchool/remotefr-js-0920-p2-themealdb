4714e632bac2490303dc5e7f2f7b36cec7f14d5f
Miguel Santos <migueljsantos27@gmail.com>
2020-10-27 15:32:57 +0100

    add slider contant

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import React from 'react';
 import './App.css';
 import './normalize.css';
+import Slider from './components/Slider';
 
 function App() {
   return (
     <div className="App">
-
+      <Slider />
     </div>
   );
 }
---
+++ src/components/Slider.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Slider() {
+  return (
+    <div className="slider">
+      <p>Slider</p>
+    </div>
+  );
+}
+
+export default Slider;
