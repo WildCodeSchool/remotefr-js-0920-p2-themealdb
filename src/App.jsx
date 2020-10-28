59dc3f538554ba984ee5617a16b3357140b986db
Benoit Hubert <benoit.hubert@wildcodeschool.com>
2020-10-28 16:20:55 +0100

    Downgrade react-scripts to 3.4.4 (app runs despite ESLint errors)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
+import React from 'react';
 import './App.css';
 import './normalize.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
