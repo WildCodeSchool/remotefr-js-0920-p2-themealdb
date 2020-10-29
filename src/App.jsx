fca92dfeceb1496b92b7c2e372e91fd89e473958
Cédric Priouret <cedric.priouret@gmail.com>
2020-10-29 09:29:02 +0100

    Add previous version of ArticleList

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 import './normalize.css';
+import ArticleList from './components/ArticleList';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <ArticleList />
+    </div>
+  );
 }
 
 export default App;
