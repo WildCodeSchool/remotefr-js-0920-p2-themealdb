3ef440dc5eab869ea6f9c25d3595380c1948e242
MatisseFMLR <matissefamularo@gmail.com>
2020-11-09 11:40:42 +0100

    Functional link on article list

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,20 +1,22 @@
 /* eslint-disable no-console */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Article from './Article';
 import './Article.css';
 
 function ArticleList({ results }) {
-
   return (
     <div className="ArticleList">
       {results.map((item) => (
-        <Article
-          key={item.idMeal}
-          strTags={item.strTags || 'NO-TAG'}
-          strMealThumb={item.strMealThumb}
-          strMeal={item.strMeal}
-        />
+        <Link to={`recipe/${item.idMeal}`}>
+          <Article
+            key={item.idMeal}
+            strTags={item.strTags || 'NO-TAG'}
+            strMealThumb={item.strMealThumb}
+            strMeal={item.strMeal}
+          />
+        </Link>
       ))}
     </div>
   );
