8a85f0d6e44ca31871f6822f75b37fe954a452f1
MatisseFMLR <matissefamularo@gmail.com>
2020-11-09 10:57:28 +0100

    Fixed loop API request and put initial recipe state to null

--- src/components/RecipePage.jsx
+++ src/components/RecipePage.jsx
@@ -2,55 +2,58 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function RecipePage({ recipe, getRecipe, link }) {
-  const ingredientsArray = [
-    recipe.strIngredient1,
-    recipe.strIngredient2,
-    recipe.strIngredient3,
-    recipe.strIngredient4,
-    recipe.strIngredient5,
-    recipe.strIngredient6,
-    recipe.strIngredient7,
-    recipe.strIngredient8,
-    recipe.strIngredient9,
-    recipe.strIngredient10,
-    recipe.strIngredient11,
-    recipe.strIngredient12,
-    recipe.strIngredient13,
-    recipe.strIngredient14,
-    recipe.strIngredient15,
-    recipe.strIngredient16,
-    recipe.strIngredient17,
-    recipe.strIngredient18,
-    recipe.strIngredient19,
-    recipe.strIngredient20,
-  ];
+  if (recipe === null) {
+    getRecipe(Number(link));
+    return <p>Loading</p>;
+  }
+  {
+    const ingredientsArray = [
+      recipe.strIngredient1,
+      recipe.strIngredient2,
+      recipe.strIngredient3,
+      recipe.strIngredient4,
+      recipe.strIngredient5,
+      recipe.strIngredient6,
+      recipe.strIngredient7,
+      recipe.strIngredient8,
+      recipe.strIngredient9,
+      recipe.strIngredient10,
+      recipe.strIngredient11,
+      recipe.strIngredient12,
+      recipe.strIngredient13,
+      recipe.strIngredient14,
+      recipe.strIngredient15,
+      recipe.strIngredient16,
+      recipe.strIngredient17,
+      recipe.strIngredient18,
+      recipe.strIngredient19,
+      recipe.strIngredient20,
+    ];
 
-  const measuresArray = [
-    recipe.strMeasure1,
-    recipe.strMeasure2,
-    recipe.strMeasure3,
-    recipe.strMeasure4,
-    recipe.strMeasure5,
-    recipe.strMeasure6,
-    recipe.strMeasure7,
-    recipe.strMeasure8,
-    recipe.strMeasure9,
-    recipe.strMeasure10,
-    recipe.strMeasure11,
-    recipe.strMeasure12,
-    recipe.strMeasure13,
-    recipe.strMeasure14,
-    recipe.strMeasure15,
-    recipe.strMeasure16,
-    recipe.strMeasure17,
-    recipe.strMeasure18,
-    recipe.strMeasure19,
-    recipe.strMeasure20,
-  ];
+    const measuresArray = [
+      recipe.strMeasure1,
+      recipe.strMeasure2,
+      recipe.strMeasure3,
+      recipe.strMeasure4,
+      recipe.strMeasure5,
+      recipe.strMeasure6,
+      recipe.strMeasure7,
+      recipe.strMeasure8,
+      recipe.strMeasure9,
+      recipe.strMeasure10,
+      recipe.strMeasure11,
+      recipe.strMeasure12,
+      recipe.strMeasure13,
+      recipe.strMeasure14,
+      recipe.strMeasure15,
+      recipe.strMeasure16,
+      recipe.strMeasure17,
+      recipe.strMeasure18,
+      recipe.strMeasure19,
+      recipe.strMeasure20,
+    ];
 
-  return (
-    getRecipe(Number(link)),
-    (
+    return (
       <div className="RecipePage">
         <h1>{recipe.strMeal}</h1>
         <div className="Recipe">
@@ -84,8 +87,8 @@ function RecipePage({ recipe, getRecipe, link }) {
           </div>
         </div>
       </div>
-    )
-  );
+    );
+  }
 }
 
 RecipePage.propTypes = {
