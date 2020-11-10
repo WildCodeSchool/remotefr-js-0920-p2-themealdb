20ffda67828b872ed63d7aca69f941515e70e01c
Miguel Santos <migueljsantos27@gmail.com>
2020-11-10 16:24:21 +0100

    add API for SLider

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,33 @@ class App extends React.Component {
     super(props);
     this.state = {
       recipe: null,
+      img: [],
     };
     this.getRecipe = this.getRecipe.bind(this);
+  }
+
+  componentDidMount() {
+    const one = 'https://www.themealdb.com/api/json/v1/1/random.php';
+    axios
+      .all([
+        axios.get(one).then((response) => response.data),
+        axios.get(one).then((response) => response.data),
+        axios.get(one).then((response) => response.data),
+        axios.get(one).then((response) => response.data),
+        axios.get(one).then((response) => response.data),
+        axios.get(one).then((response) => response.data),
+        axios.get(one).then((response) => response.data),
+        axios.get(one).then((response) => response.data),
+        axios.get(one).then((response) => response.data),
+        axios.get(one).then((response) => response.data),
+        axios.get(one).then((response) => response.data),
+      ])
+      .then((data) => {
+        const imgData = data.map((item) => item.meals[0].strMealThumb);
+        this.setState({
+          img: imgData,
+        });
+      });
   }
 
   getRecipe(id) {
@@ -32,30 +57,27 @@ class App extends React.Component {
           recipe: data.meals[0],
         });
       });
+    this.getSlider = this.getSlider.bind(this);
   }
 
   render() {
     const breakPoints = [
-      { width: 1, slideShow: 1 },
-      { width: 500, slideShow: 2 },
-      { width: 768, slideShow: 3 },
-      { width: 1200, slideShow: 4 },
+      { width: 1, itemsToScroll: 1, itemsToShow: 1 },
+      { width: 500, itemsToScroll: 2, itemsToShow: 1 },
+      { width: 768, itemsToScroll: 3, itemsToShow: 3 },
+      { width: 1200, itemsToScroll: 4, itemsToShow: 3 },
     ];
     const { recipe } = this.state;
+    const { img } = this.state;
     return (
       <div className="App">
         <Navbar />
         <Switch>
           <Route exact path="/">
             <Carousel breakPoints={breakPoints}>
-              <Slider img="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" />
-              <Slider img="https://daks2k3a4ib2z.cloudfront.net/54f8d2b973ee3d0b0579571c/550af79042dd862c6ca48fa0_food-meat-potatoes-steak-130747-480x320.jpg" />
-              <Slider img="https://www.bipbippizza.ch/wp-content/uploads/2016/10/pizza2.jpg" />
-              <Slider img="https://media.istockphoto.com/photos/selection-of-american-food-picture-id931308812?k=6&m=931308812&s=612x612&w=0&h=bIbWqdW3LEc9Q5_WnqYG0blqrKp9zW7Qc36Gq35FTaE=" />
-              <Slider img="https://media-manager.noticiasaominuto.com/1920/5e3157c6297fa.jpg" />
-              <Slider img="https://www.teleculinaria.pt/wp-content/uploads/2018/02/massada-de-peixe-rapida-e1519644831736.jpg" />
-              <Slider img="https://thumbs.dreamstime.com/b/chicken-jalfrazy-indian-food-recipe-spices-wooden-table-92742377.jpg" />
-              <Slider img="https://www.recettes-asselin.com/img/fondue-chinoise.jpg" />
+              {img.map((picture) => (
+                <Slider key={picture} img={picture} />
+              ))}
             </Carousel>
             <SearchBar />
             <ContactForm />
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Slider.css';
 
-const Slider = ({ img }) => (
-  <div className="slider">
-    <img src={img} alt="food" />
-  </div>
-);
+const Slider = (props) => {
+  const { img } = props;
+  return (
+    <div className="slider">
+      <img src={img} alt={img} />
+    </div>
+  );
+};
 Slider.propTypes = {
   img: PropTypes.string.isRequired,
 };
