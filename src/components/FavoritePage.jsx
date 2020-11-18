ddfbaa4979362399a8ad677883d79d644890d665
MatisseFMLR <matissefamularo@gmail.com>
2020-11-18 11:22:11 +0100

    Component did mount in FavoritePage working, getting an array of recipes from just some id in state

--- src/components/FavoritePage.jsx
+++ src/components/FavoritePage.jsx
@@ -1,7 +1,42 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
+import axios from 'axios';
+// import ArticleList from './ArticleList';
 
-const FavoritePage = () => {
-  return <p>This is a favorites page</p>;
-};
+class FavoritePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites: ['52772', '52773'],
+      // favArray: null,
+    };
+  }
+
+  componentDidMount() {
+    console.log(this.state.favorites);
+    axios
+      .all(
+        this.state.favorites.map((fav) =>
+          axios
+            .get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${fav}`)
+            .then((res) => res.data),
+        ),
+      )
+      .then((data) => {
+        return console.log(data);
+      });
+  }
+
+  render() {
+    // const { favArray } = this.state;
+
+    return (
+      <div>
+        <p>This is a favorites page</p>
+        {/* <ArticleList fav={favArray} /> */}
+      </div>
+    );
+  }
+}
 
 export default FavoritePage;
